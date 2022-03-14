c1b5792b4c9cfaaedcf72d1e5db57eef57de5085
azzmnrwebdev <azzmnrwebdev@gmail.com>
2022-03-14 19:41:12 +0700

    task pertemuan ke 10

--- setTimeout.js
+++
@@ -1,7 +0,0 @@
-// setTimeout | Asynchronous Programming Javascript
-console.log("----- setTimeout | Asynchronous Programming -----");
-console.log("Satu");
-
-setTimeout(() => console.log("Dua"), 3000);
-
-console.log("Tiga");
